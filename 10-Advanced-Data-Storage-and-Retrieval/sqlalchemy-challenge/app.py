a4692a5b4146282668f01a9d265dfe7fc76cdfb7
nswehli <53152702+nswehli@users.noreply.github.com>
2019-11-25 01:21:18 -0500

    SQLAlchemy HW

--- 10-Advanced-Data-Storage-and-Retrieval/sqlalchemy-challenge/app.py
+++ 10-Advanced-Data-Storage-and-Retrieval/sqlalchemy-challenge/app.py
@@ -1,9 +1,12 @@
 import numpy as np
-
+import datetime as dt
+from datetime import date
+from datetime import datetime
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+###
 
 from flask import Flask, jsonify
 engine = create_engine("sqlite:///hawaii.sqlite")
@@ -19,7 +22,15 @@ app = Flask(__name__)
 def welcome():
     """List all available api routes."""
     print("Server received request for 'Home' page...")
-    return "Welcome to my 'Home' page!"
+    return (f"Welcome to the  Climate App!<br/> <br/>"
+    f"Available Routes:<br/><br/>"
+    f"/api/v1.0/precipitation<br/>"
+    f"/api/v1.0/stations<br/>"
+    f"/api/v1.0/tobs<br/>"
+    f"/api/v1.0/startdate<br/>"
+    f"/api/v1.0/startdate/enddate"
+
+    )
 
 ###
 
@@ -47,6 +58,63 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def stations():
     session = Session(engine)
+    results = session.query(Station.id
+                        , Station.station
+                        , Station.name
+                        , Station.latitude
+                        ,Station.longitude
+                        ,Station.elevation
+                        ).all()
+    session.close()
+    return jsonify(results)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    ### Calculating dates
+    LatestDate=session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    LatestDate=datetime.strptime(LatestDate[0], '%Y-%m-%d')
+    yr_ago = LatestDate - dt.timedelta(days=365)
+
+    ### Querying for data of last 12 months
+
+    results = session.query(Measurement.date
+                            ,Measurement.tobs
+                       ).filter(Measurement.date >= yr_ago).all()
+    session.close()
+    return jsonify(results)
+    
+     
+@app.route("/api/v1.0/<start>")
+  
+def start(start):
+        session = Session(engine)
+        startDate=datetime.strptime(start, '%Y-%m-%d')
+        #endDate=datetime.strptime("2016-02-20", '%Y-%m-%d')
+
+        results = session.query(Measurement.date,func.min(Measurement.tobs).label("Min Temp")
+         ,func.avg(Measurement.tobs).label("Avg Temp")
+         ,func.max(Measurement.tobs).label("Max Temp")).group_by(Measurement.date).filter(Measurement.date >= start).all()
+                        
+        session.close()
+        return jsonify(results)
+
+
+     
+@app.route("/api/v1.0/<start>/<end>")
+  
+def date(start,end):
+        session = Session(engine)
+        startDate=datetime.strptime(start, '%Y-%m-%d')
+        endDate=datetime.strptime(end, '%Y-%m-%d')
+
+        results = session.query(Measurement.date,func.min(Measurement.tobs).label("Min Temp")
+         ,func.avg(Measurement.tobs).label("Avg Temp")
+         ,func.max(Measurement.tobs).label("Max Temp")).group_by(Measurement.date).filter(Measurement.date >= startDate,Measurement.date <= endDate ).all()
+                        
+        session.close()
+        return jsonify(results)
+
 
     
 
